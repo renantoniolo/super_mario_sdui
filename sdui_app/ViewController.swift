94766f2a4f8a7db58f92450976c40e76b5d8c965
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-01-21 10:04:45 -0300

    Initial Commit

---
+++ sdui_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sdui_app
+//
+//  Created by Renan Toniolo Rocha on 21/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sdui_appUITests/sdui_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  sdui_appUITestsLaunchTests.swift
+//  sdui_appUITests
+//
+//  Created by Renan Toniolo Rocha on 21/01/25.
+//
+
+import XCTest
+
+final class sdui_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
